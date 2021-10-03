355e484a26511a9f0d4b9b6e4d2e7e56096ec6ec
Antoine Washington <antoine.washington98@gmail.com>
2021-10-03 13:36:48 -0400

    playingwithdata to make a structured array of all the hartley scan metadatas

---
+++ playingwithdata.py
@@ -0,0 +1,43 @@
+#Antoine still
+#gonna play around with structured arrays because
+#i think itd be good to store the metadata of each of these scans
+#in a single place for easy access
+
+import numpy as np
+
+scan_str = np.loadtxt("/home/antojr/stash/datatxt/broly.txt",dtype=str,skiprows=1,usecols=(0,2,4))
+#scan_flo = np.loadtxt("/home/antojr/stash/datatxt/broly.txt",dtype=float,skiprows=1,usecols=(1,3,5,6))
+dark_tem = np.loadtxt("/home/antojr/stash/datatxt/dark_temp_v2.dat",dtype=float,skiprows=1,usecols=(0,1,2,3,4,5,7))
+dark_str = np.loadtxt("/home/antojr/stash/datatxt/dark_temp_v2.dat",dtype=str,skiprows=1,usecols=(6))
+coords_num = np.loadtxt("/home/antojr/stash/datatxt/ir_coords.txt",dtype=float,usecols=(0,2,3))
+coords_eid = np.loadtxt("/home/antojr/stash/datatxt/ir_coords.txt",dtype=str,usecols=(1))
+
+#for each scan there is:
+#julian date, smooth temp, dark level, best fit level, exposure time, doy, exposure id, outlier flag,
+#filename, long date (skip), ir_coords
+
+typex = np.dtype([('julian date','f8'),('DOY','i4'),('exposure id','U8'),('temperature','f8'), ('dark level','f8'),('dark best fit','f8'),('x-nucleus','f8'), ('y-nucleus','f8'), ('exposure time','f8'),('outlier flag','?'),('filename','U26')])
+
+dats = []
+for i in range(len(dark_tem)):
+    dats.append(( dark_tem[i,0] , int(dark_tem[i,5]), dark_str[i], dark_tem[i,1], dark_tem[i,2], dark_tem[i,3] , -99., -99., dark_tem[i,4] , dark_tem[i,6], scan_str[i,0]))
+a = np.array( dats, dtype = typex )
+# we will go through every row
+for i in range(len(coords_num)):
+    doy, xn, yn, = coords_num[i]
+    ein = coords_eid[i]
+    #will go over every row in the structured array for matching
+    matched = False
+    for j in range(len(a)):
+        doy_a = a['DOY'][j]
+        exp_a = a['exposure id'][j]
+        if (doy_a==doy) and (exp_a==ein): #day of year and exposure ids match
+            #will need to fill in xn and yn
+            a['x-nucleus'][j] = xn
+            a['y-nucleus'][j] = yn
+            #okay it's over
+            matched = True
+        pass
+    #if matched == False:
+    #    print(f"no match found for {doy} {ein}")
+    pass
