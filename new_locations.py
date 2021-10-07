a9ee80e9d618dfe000530d7eeab4ea51a3c1005b
Antoine Washington <antoine.washington98@gmail.com>
2021-10-07 10:50:55 -0400

    fixed error in new nucleus locations

--- new_locations.py
+++ new_locations.py
@@ -15,7 +15,7 @@ ynew = [ float(a['filename'][i][19:22]) - ys[i] for i in range(len(ys)) ]
 outt = open("nucleus_locations_v2.txt","w")
 for i in range(len(ys)):
     if (ys[i] - -99. < 0.1) or (ynew[i] < 0.):
-        outt.write(f"{jd[i]} -99 -99.\n")
+        outt.write(f"{jd[i]} -99. -99.\n")
     else:
         outt.write(f"{jd[i]} {xs[i]} {ynew[i]}\n")
     pass
