8b13bfc99e8aeb05fff3c558534c5ffb05d811e5
Antoine Washington <antoine.washington98@gmail.com>
2022-08-02 17:52:12 -0400

    gonna do some work locally so heres a push so i can pull

--- cubestats.py
+++ cubestats.py
@@ -7,6 +7,7 @@ from astropy.io import fits as fit
 from cometmeta import a
 from datafunctions import selector, selector_prompt
 
+## unload the cube and its data
 select = selector_prompt(default='307 4000011')
 dato = fit.open( a['directory path'][select] + '/cube_gasmaps_final_enhance_v6.fit')
 #wavo = fit.open
@@ -16,6 +17,7 @@ colos, ysize, xsize1 = gasmaps.shape
 imag = gasmaps[:,:,170:]
 xsize2 = imag.shape[2]
 
+## derive some stats from that mans
 mean = np.nanmean(imag,axis=(1,2))
 #print(mean.shape)
 #h2o_mean, co2_mean, dus_mean = np.nanmean(imag[0,:,:]), np.nanmean(imag[1,:,:]), np.nanmean(imag[2,:,:])
@@ -25,10 +27,17 @@ var = stan**2
 maxx = np.nanmax(imag,axis=(1,2))
 for i in (0,1,2):
     print(mean[i],medi[i],stan[i],var[i],maxx[i])
+
+## debugging, cross-checking, etc
+vall = gasmaps[0,14,202]
+print(vall)
+
 fig,ax = plt.subplots()
 fig.figsize=(10,5.6)
 fig.dpi=140
-ax.hist(imag[0,:,:])
-ax.vlines((mean[0], mean[0]+var[0]), ymin=0,ymax=50)
+ax.hist(gasmaps[1,:,:].ravel(),bins=350)
+#ax.vlines((mean[0], mean[0]+var[0]), ymin=0,ymax=50)
+ax.set_yscale('log')
+ax.set_ylim(1,None)
 plt.show()
 
