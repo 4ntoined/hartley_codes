e49065345deb0ef28d090790ecea2f6c017cab4f
Antoine Washington <antoine.washington98@gmail.com>
2022-08-02 20:47:09 -0400

    it came up (i brought it up)

--- cubestats.py
+++ cubestats.py
@@ -24,20 +24,34 @@ def unloadCube(scan_index, cubename='', wavename=''):
     #imag = gasmaps[:,:,170:]
     #xsize2 = imag.shape[2]
 
-def plotHist(gas):
+def plotHist(gas,smallmap = False):
     #gas = 0 for h2o, = 1 for co2
+    global gasmap, imag
     fig,ax = plt.subplots()
     fig.figsize=(10,5.6)
     fig.dpi=140
-    ax.hist(gasmaps[gas,:,:].ravel(),bins=350)
-    #ax.vlines((mean[0], mean[0]+var[0]), ymin=0,ymax=50)
+    if smallmap:
+        mappo = imag
+    else:
+        mappo = gasmaps
+    histog = ax.hist(mappo[gas,:,:].ravel(),bins=350)
+    histo = histog[0]
+    ax.vlines( (0,mean[gas]), ymin=0.9,ymax=1.1*np.max(histo),color=('k','r'), linewidth=0.7)
     ax.set_yscale('log')
     #ax.set_ylim(,None)
+    ax.set_ylabel('Counts')
+    ax.set_xlabel('Pixel value')
+    if gas == 0:
+        ax.set_title(f"{a['julian date'][select]:.3f} | {a['DOY'][select]}.{a['exposure id'][select]} | H2O")
+    elif gas ==1:
+        ax.set_title(f"{a['julian date'][select]:.3f} | {a['DOY'][select]}.{a['exposure id'][select]} | CO2")
+    else:
+        ax.set_title("You broke this bro, don't even know if you should be seeing this")
     plt.show()
     return
 
 if __name__ == '__main__':
-    mode = input('What mode: ')
+    mode = input('What mode: ') or '1'
     if mode == '1':
         ## selecting and unloading the cube
         select = selector_prompt(default='307 4000011')
@@ -55,9 +69,9 @@ if __name__ == '__main__':
         
         #for i in (0,1,2): #i wanna just make this a big array so I can call it later
         #    print(mean[i],medi[i],stan[i],vari[i],maxx[i])
-        plotting = input('Plot? 1 for h2o, 2 for co2, else for no plotting\n:  ')
-        if plotting == '1' or plotting == '2':
-            plotHist(int(plotting))
+        plotting = input('Plot? 0 for h2o, 1 for co2, else for no plotting\n:  ')
+        if plotting == '0' or plotting == '1':
+            plotHist(int(plotting),smallmap=True)
         else:   #do nothing if input is wrong
             print('bad input')
         pass
